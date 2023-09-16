95533bf693771ac4aaf7849a8ffd4f5c55d59d0d
Ythalo Ryan <naturanatura9182@gmail.com>
2023-09-16 15:43:05 -0300

    Errors CheckUps

--- PokeApi.py
+++ PokeApi.py
@@ -3,7 +3,7 @@ from pprint import pprint
 import os
 import time
 
-nomes_pokemon = ["Pikachu", "34", "bulbasaur", "squirtle", "jigglypuff"]
+nomes_pokemon = [150]
 
 #Get current directory
 global cur_dir
@@ -19,13 +19,18 @@ class Pokemon:
         #Request Archives
         self.poke_id = poke_id
         self.poke_json = Pokemon.get_infos(self)
+        if self.poke_json is None:
+            return None
         
         #Pokemon image and infromatios
         self.poke_name = self.poke_json['name'].capitalize()
         self.poke_image = Pokemon.poke_photo(self)
+        if self.poke_image is None:
+            return None
         
         #Main folder
-        self.poke_folder = os.path.join(cur_dir, 'pokemons', self.poke_name)
+        self.alpha_poke_folder = os.path.join(cur_dir, 'pokemons', self.poke_name[0])
+        self.poke_folder = os.path.join(self.alpha_poke_folder, self.poke_name)
         
         #Create a folder for pokemon and save
         Pokemon.make_poke_dir(self)
@@ -38,7 +43,12 @@ class Pokemon:
     def poke_photo(self):
         #Get the link from the json link
         photo_link = self.poke_json['sprites']['other']['home']['front_default']
-        
+        if photo_link is None:
+            photo_link = self.poke_json['sprites']['other']['official-artwork']['front_default']
+            if photo_link is None:
+                print('Sprite Not Found')
+                return None
+  
         #Get the photo
         res = requests.get(photo_link)
         if res.status_code == 200:
@@ -48,7 +58,12 @@ class Pokemon:
     
     
     def make_poke_dir(self):
-        #Make the fodler with Pokemon name
+        #Make Alphabetic folder
+        alpha_poke_folder = self.alpha_poke_folder
+        if not os.path.exists(alpha_poke_folder):
+            os.mkdir(alpha_poke_folder)           
+            
+        #Make the folder with Pokemon name
         poke_folder = self.poke_folder
         if not os.path.exists(poke_folder):
             os.mkdir(poke_folder)
@@ -59,7 +74,6 @@ class Pokemon:
             print(f'The folder {self.poke_name} already exists.')
             
             
-    
     def save_poke_infos(self):
         poke_folder = self.poke_folder
            
@@ -73,19 +87,20 @@ class Pokemon:
     def get_infos(self):
         #Get informations from the api
         try:
-            res = requests.get(f"https://pokeapi.co/api/v2/pokemon/{self.poke_id.lower()}")
+            res = requests.get(f"https://pokeapi.co/api/v2/pokemon/{self.poke_id.lower()}/")
             
             if res.status_code == 200:
                 return res.json()
             else:
                 print(f"A solicitação falhou com o código de status: {res.status_code}")
+                return None
                 
         except requests.exceptions.RequestException as e:
             print(f"Um erro ocorreu durante a solicitação: {e}")
-        
+                    
         
 for pokemon in nomes_pokemon:
-    Pokemon(pokemon)
+    Pokemon(str(pokemon))
     
 print('exiting...')
 time.sleep(3)
--- teste.py
+++ teste.py
@@ -1,23 +1,11 @@
 import requests
 import os
+import json
 
-# URL da imagem que você deseja baixar
-url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/25.png"
+res = requests.get('https://pokeapi.co/api/v2/pokemon/10271/',
+                   params={'limit': '10000'})
 
-# Envie uma solicitação GET para a URL para obter o conteúdo da imagem
-response = requests.get(url)
-
-# Verifique se a solicitação foi bem-sucedida (código de status 200)
-if response.status_code == 200:
-    # Especifique o caminho local onde você deseja salvar a imagem
-    
-    caminho_local = os.getcwd()
-    
-
-    # Abra o arquivo em modo binário para gravar a imagem
-    with open(caminho_local, "wb") as file:
-        file.write(response.content)
-
-    print(f'Imagem salva em "{caminho_local}"')
-else:
-    print("Falha ao baixar a imagem. Código de status:", response.status_code)
+print(res)
+print(res.json())
+with open('testes.json', 'w') as teste:
+    json.dump(res.json(), teste, indent=4)
