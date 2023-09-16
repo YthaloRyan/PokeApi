2255be3711d881254723c4b76fa6cf77694d6196
Ythalo Ryan <naturanatura9182@gmail.com>
2023-09-16 00:26:41 -0300

    Added the save images

--- PokeApi.py
+++ PokeApi.py
@@ -1,37 +1,45 @@
 import requests
 from pprint import pprint
-import json
 import os
+import time
 
 nomes_pokemon = ["Pikachu", "34", "bulbasaur", "squirtle", "jigglypuff"]
 
-#get current directory
+#Get current directory
 global cur_dir
 cur_dir = os.getcwd()
 
-#create the directory for pokemons
+#Create the directory for pokemons
 pokes_dir = os.path.join(cur_dir,'pokemons')
 if not os.path.exists(pokes_dir):
     os.mkdir(pokes_dir)
 
 class Pokemon:
     def __init__(self,poke_id):
+        #Request Archives
         self.poke_id = poke_id
         self.poke_json = Pokemon.get_infos(self)
+        
+        #Pokemon image and infromatios
         self.poke_name = self.poke_json['name'].capitalize()
         self.poke_image = Pokemon.poke_photo(self)
         
-        Pokemon.make_poke_dir(self)
+        #Main folder
+        self.poke_folder = os.path.join(cur_dir, 'pokemons', self.poke_name)
         
-       
+        #Create a folder for pokemon and save
+        Pokemon.make_poke_dir(self)
+          
         #temp
-        with open('testes.json', 'w') as teste:
-            json.dump(self.poke_json, teste, indent=4)
+        # with open('testes.json', 'w') as teste:
+        #     json.dump(self.poke_json, teste, indent=4)
             
         
     def poke_photo(self):
+        #Get the link from the json link
         photo_link = self.poke_json['sprites']['other']['home']['front_default']
         
+        #Get the photo
         res = requests.get(photo_link)
         if res.status_code == 200:
             return res.content
@@ -40,18 +48,30 @@ class Pokemon:
     
     
     def make_poke_dir(self):
-        poke_folder = os.path.join(cur_dir, 'pokemons', self.poke_name)
+        #Make the fodler with Pokemon name
+        poke_folder = self.poke_folder
         if not os.path.exists(poke_folder):
             os.mkdir(poke_folder)
+            
+            #Save all information
+            Pokemon.save_poke_infos(self)
         else:
             print(f'The folder {self.poke_name} already exists.')
             
+            
     
     def save_poke_infos(self):
-        pass
+        poke_folder = self.poke_folder
+           
+        #Save image
+        with open(f'{poke_folder}\{self.poke_name}.png', "wb") as file:
+            file.write(self.poke_image)
+            
+        print(f'{self.poke_name} saved successfully.')
     
     
-    def get_infos(self): 
+    def get_infos(self):
+        #Get informations from the api
         try:
             res = requests.get(f"https://pokeapi.co/api/v2/pokemon/{self.poke_id.lower()}")
             
@@ -66,3 +86,6 @@ class Pokemon:
         
 for pokemon in nomes_pokemon:
     Pokemon(pokemon)
+    
+print('exiting...')
+time.sleep(3)
--- teste.py
+++ teste.py
@@ -1,18 +1,23 @@
+import requests
 import os
 
-# Obtém o diretório atual do arquivo .py
-diretorio_atual = os.getcwd()
-print(diretorio_atual)
-# Nome da nova pasta que você deseja criar
-nome_pasta = "minha_pasta"
+# URL da imagem que você deseja baixar
+url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/25.png"
 
-# Caminho completo para a nova pasta
-caminho_pasta = os.path.join(diretorio_atual, nome_pasta)
+# Envie uma solicitação GET para a URL para obter o conteúdo da imagem
+response = requests.get(url)
 
-# # Verifica se a pasta já existe
-# if not os.path.exists(caminho_pasta):
-#     # Cria a pasta se ela não existir
-#     os.mkdir(caminho_pasta)
-#     print(f'A pasta "{nome_pasta}" foi criada com sucesso no diretório atual.')
-# else:
-#     print(f'The folder "{no}" já existe no diretório atual.')
+# Verifique se a solicitação foi bem-sucedida (código de status 200)
+if response.status_code == 200:
+    # Especifique o caminho local onde você deseja salvar a imagem
+    
+    caminho_local = os.getcwd()
+    
+
+    # Abra o arquivo em modo binário para gravar a imagem
+    with open(caminho_local, "wb") as file:
+        file.write(response.content)
+
+    print(f'Imagem salva em "{caminho_local}"')
+else:
+    print("Falha ao baixar a imagem. Código de status:", response.status_code)
